3fe68742711542059be70245d315ddde6c255d89
akshayrachakonda <akshayrachakonda5@gmail.com>
2025-02-17 10:49:06 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
-import Classroom from "./pages/Classroom";
-import Assesment from "./pages/Assesment";
-import Store from "./pages/Store";
-import Calender from "./pages/Calender";
-import News from "./pages/News";
-import Blog from "./pages/Blog";
-import PageNotFound from "./pages/PageNotFound";
-import Profile from "./pages/Profile";
-import MySettings from "./pages/MySettings";
-import Myaccount from "./pages/Myaccount";
+// import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Dashboard from "./pages/Dashboard";
+// import Classroom from "./pages/Classroom";
+// import Assesment from "./pages/Assesment";
+// import Store from "./pages/Store";
+// import Calender from "./pages/Calender";
+// import News from "./pages/News";
+// import Blog from "./pages/Blog";
+// import PageNotFound from "./pages/PageNotFound";
+// import Profile from "./pages/Profile";
+// import MySettings from "./pages/MySettings";
+// import Myaccount from "./pages/Myaccount";
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//         <Navbar/>
+//         <Routes>
+//            <Route path='/' element={<Dashboard/>}/>
+//            <Route path='/classroom' element={<Classroom/>}/>
+//            <Route path='/assesment' element={<Assesment/>}/>
+//            <Route path='/store' element={<Store/>}/>
+//            <Route path='/calender' element={<Calender/>}/>
+//            <Route path='/news' element={<News/>}/>
+//            <Route path='/blog' element={<Blog/>}/>
+//            <Route path='*' element={<PageNotFound/>}/>
+//            <Route path="/profile" element={<Profile/>}>
+//                   <Route path="/profile/myaccount" element={<Myaccount/>}/>
+//                   <Route path="/profile/mysettings" element={<MySettings/>}/>
+//            </Route>
+//         </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './CRUD Components/Home'
+import Create from './CRUD Components/Create'
+import Edit from './CRUD Components/Edit'
 const App = () => {
   return (
     <BrowserRouter>
-        <Navbar/>
-        <Routes>
-           <Route path='/' element={<Dashboard/>}/>
-           <Route path='/classroom' element={<Classroom/>}/>
-           <Route path='/assesment' element={<Assesment/>}/>
-           <Route path='/store' element={<Store/>}/>
-           <Route path='/calender' element={<Calender/>}/>
-           <Route path='/news' element={<News/>}/>
-           <Route path='/blog' element={<Blog/>}/>
-           <Route path='*' element={<PageNotFound/>}/>
-           <Route path="/profile" element={<Profile/>}>
-                  <Route path="/profile/myaccount" element={<Myaccount/>}/>
-                  <Route path="/profile/mysettings" element={<MySettings/>}/>
-           </Route>
-        </Routes>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/create' element = {<Create/>}/>
+        <Route path='/edit/:userId' element = {<Edit/>}/>
+      </Routes>
     </BrowserRouter>
   )
 }
